de6989aab4735ddf7d2af04ab5c24fc9f99aeeb8
study <study@chaimac.local>
2015-09-19 21:36:58 -0700

    Initial Commit

---
+++ score5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  score5
+//
+//  Created by study on 2015. 9. 19..
+//  Copyright (c) 2015년 study. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
